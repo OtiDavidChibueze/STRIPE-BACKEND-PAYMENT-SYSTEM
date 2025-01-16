5c896d2ea782e052c263ec63956a5f7d12adb473
OtiDavidChibueze <davidchibueze47@gmail.com>
2025-01-16 08:44:17 -0800

    added

---
+++ server.js
@@ -0,0 +1,74 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const STRIPE_SECRET_KEY = process.env.stripe_secret_key;
+const STRIPE_BASE_URL = process.env.stripe_base_url;
+
+async function createProduct(productName) {
+  try {
+    const response = await fetch(`${STRIPE_BASE_URL}/products`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${STRIPE_SECRET_KEY}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `name=${productName}`,
+    });
+
+    const product = await response.json();
+    return product;
+  } catch (err) {
+    console.log("Error at create product", err);
+  }
+}
+
+async function addPriceToProduct(productId, amount, currency) {
+  try {
+    const response = await fetch(`${STRIPE_BASE_URL}/prices`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${STRIPE_SECRET_KEY}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `unit_amount=${amount}&currency=${currency}&product=${productId}`,
+    });
+
+    const price = await response.json();
+    return price;
+  } catch (err) {
+    console.log("Error at add price to product", err);
+  }
+}
+
+async function createPaymentLink(priceId) {
+  try {
+    const response = await fetch(`${STRIPE_BASE_URL}/checkout/sessions`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${STRIPE_SECRET_KEY}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `mode=payment&payment_method_types[0]=card&success_url=http://www.your-website.com/success&cancel_url=http://www.your-website.com/cancel&line_items[0][price]=${priceId}&line_items[0][quantity]=1`,
+    });
+
+    const paymentLink = await response.json();
+    return paymentLink;
+  } catch (err) {
+    console.log("Error at createPaymentLink", err);
+  }
+}
+
+(async () => {
+  try {
+    const product = await createProduct("TESTING");
+    console.error("product created", product);
+
+    const price = await addPriceToProduct(product.id, 1000, "USD");
+    console.error("price added to product", price);
+
+    const paymentLink = await createPaymentLink(price.id);
+    console.error("payment link:", paymentLink);
+  } catch (error) {
+    console.error("Exception Errors", error);
+  }
+})();
